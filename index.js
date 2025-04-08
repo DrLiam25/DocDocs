f3f066b100c28e0a43a6cc3e63d43ce9237a2140
DrLiam25 <liamPartridge04@gmail.com>
2025-04-08 11:18:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,287 @@
+var docFirstNames = [
+  "Liam", "Emma", "Noah", "Olivia", "Elijah", "Ava", "James", "Isabella", "Sophia", "Benjamin", 
+  "Mia", "Lucas", "Charlotte", "Mason", "Amelia", "Ethan", "Harper", "Alexander", "Evelyn", "Henry", 
+  "Abigail", "Jacob", "Ella", "Michael", "Scarlett", "Daniel", "Emily", "Logan", "Elizabeth", 
+  "Jackson", "Camila", "Sebastian", "Luna", "Aiden", "Sofia", "Matthew", "Avery", "Samuel", "Mila", 
+  "David", "Aria", "Joseph", "Layla", "Carter", "Chloe", "Owen", "Zoey", "Wyatt", "Nora", 
+  "John", "Riley", "Leo", "Lily", "Isaac", "Ellie", "Dylan", "Hazel", "Jayden", "Grace", 
+  "Gabriel", "Hannah", "Luke", "Violet", "Anthony", "Aurora", "Grayson", "Penelope", "Jack", 
+  "Lillian", "Julian", "Addison", "Levi", "Stella", "Christopher", "Isla", "Joshua", "Lucy", 
+  "Andrew", "Natalie", "Lincoln", "Paisley", "Hudson", "Savannah", "Nathan", "Brooklyn", "Thomas", 
+  "Bella", "Ezra", "Claire", "Aaron", "Skylar", "Adrian", "Aaliyah", "Hunter", "Elena", 
+  "Nolan", "Leah", "Caleb", "Arianna", "Ryan", "Audrey", "Asher", "Allison", "Isaiah", "Sarah", 
+  "Charles", "Nevaeh", "Josiah", "Anna", "Christian", "Caroline", "Kennedy", "Eli", "Genesis", 
+  "Jonathan", "Hailey", "Connor", "Autumn", "Easton", "Eva", "Cameron", "Sadie", "Ezekiel", 
+  "Piper", "Colton", "Naomi", "Jeremiah", "Ruby", "Angel", "Serenity", "Roman", "Alice", 
+  "Jaxon", "Quinn", "Weston", "Delilah", "Luis", "Ivy", "Miles", "Cora", "Dominic", "Maria", 
+  "Vincent", "Elise", "Myles", "Adeline", "Diego", "Jasmine", "Micah", "Ayla", "Zion", "Nina", 
+  "Harrison", "Freya", "Jude", "Emery", "Gavin", "Juniper", "Axel", "Daisy", "Carlos", "Sienna", 
+  "Max", "Zara", "Zephyr", "Callista", "Oberon", "Seraphina", "Casimir", "Eulalia", "Lucian", 
+  "Ondine", "Balthazar", "Thalassa", "Elio", "Isolde", "Peregrine", "Vespera", "Tiberius", 
+  "Nyx", "Soren", "Zuleika", "Ignatius", "Selene", "Lazaro", "Artemisia", "Azriel", "Sapphira", 
+  "Octavian", "Xanthe", "Quillon", "Evadne", "Calix", "Liora", "Amadeus", "Morwenna", 
+  "Osiris", "Yvaine", "Altair", "Melisande", "Endymion", "Ione", "Cyprian", "Arianwen", 
+  "Tavish", "Eowyn", "Basil", "Elsinore", "Hadrian", "Zosime", "Viggo", "Saoirse", "Lucius", 
+  "Nimue", "Fenrir", "Calytrix", "Draven", "Eithne", "Quixote", "Idony", "Morpheus", 
+  "Lysandra", "Oisin", "Amarantha", "Vireo", "Sybella", "Oswin", "Fiorella", "Leif", "Xiomara", 
+  "Icarus", "Signe", "Ephraim", "Briseis", "Zebulon", "Mireille", "Romulus", "Calypso", 
+  "Nicodemus", "Orinthia", "Havelock", "Ferelith", "Evander", "Hypatia", "Zephyrus", "Damaris", 
+  "Tarquinius", "Elysande", "Phineas", "Astraea", "Stellan", "Celestine", "Thelonius", "Apolline", 
+  "Wolfgang", "Tindra", "Valerian", "Eurydice", "Isidore", "Thessaly", "Ozymandias", "Alouette", 
+  "Ragnar", "Thisbe", "Caradoc", "Sappho", "Theron", "Lyra", "Anaximander", "Ottilie", 
+  "Zeledon", "Araceli", "Hectorius", "Mazarine", "Zorion", "Leocadia", "Jorvik", "Cassiopeia", 
+  "Solon", "Emeraude", "Quintilian", "Orlaith", "Vladimir", "Ysolde", "Gwydion", "Alethea", 
+  "Vassilis", "Nocturne", "Caelum", "Valkyrie", "Iskander", "Cosima", "Iovianus", "Sunniva", 
+  "Galad", "Opalina", "Dionysius", "Elowen", "Lorien", "Viridiana", "Ptolemy", "Anoushka", 
+  "Calidore", "Eos", "Auberon", "Rosenwyn", "Heliodor", "Olivienne", "Serapion", "Persephone", 
+  "Thalion", "Iseult", "Esmaralda"
+];
+console.log(docFirstNames.length)
+
+
+
+const docLastNames = [
+  "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Wilson", "Taylor", "Anderson", 
+  "Thomas", "Moore", "Jackson", "Martin", "Lee", "Harris", "Clark", "Lewis", "Walker", "Hall", 
+  "Young", "Allen", "Sanchez", "Wright", "King", "Scott", "Torres", "Nguyen", "Adams", "Baker", 
+  "Gonzalez", "Nelson", "Carter", "Mitchell", "Perez", "Roberts", "Turner", "Phillips", "Campbell", 
+  "Parker", "Evans", "Edwards", "Collins", "Stewart", "Sullivan", "Foster", "Sanders", "Graham", 
+  "Hughes", "Jenkins", "Hayes", "Richardson", "Dixon", "Perkins", "Grant", "Bennett", "Chavez", 
+  "Hansen", "Harper", "Murray", "Cunningham", "Castillo", "Ross", "Kim", "Howard", "Ramos", 
+  "Vasquez", "Harrison", "Shaw", "Day", "Snyder", "Mason", "Wells", "Nichols", "Freeman", 
+  "Holland", "Ball", "O'Connor", "Meyer", "Stone", "Tucker", "Collier", "Perry", "Duarte", 
+  "Walsh", "Choi", "Glover", "McDonald", "Knox", "Klein", "Sampson", "Rosales", "Hodge", 
+  "Morris", "Santiago", "Khan", "Patel", "Nguyen", "Li", "Chen", "Zhang", "Wang", "Liu", 
+  "Tanaka", "Huang", "Sharma", "Rodriguez", "Wang", "Singh", "Garcia", "Jenkins", "Reyes", 
+  "Duarte", "Alvarez", "Kumar", "Sharma", "Chaudhry", "Patel", "Hussain", "Reddy", "Singh", 
+  "Gonzalez", "Ali", "Siddiqui", "Quintana", "Ortega", "Medina", "Cruz", "Fernandez", "Lopez", 
+  "Nunez", "Zamora", "Esquivel", "Salazar", "Mendez", "Duran", "Bautista", "Bermudez", "Jara", 
+  "Ochoa", "Cabrera", "Salinas", "Ibarra", "Cantu", "Benitez", "Mejia", "Villalobos", "Carrillo", 
+  "Santos", "Marrero", "Aguirre", "Ayala", "Romero", "Vega", "Pineda", "Zuniga", "Trevino", 
+  "Marin", "Mora", "Santana", "Luna", "Velasquez", "Rivas", "Alonzo", "Hernandez", "Rios", 
+  "Acosta", "Valenzuela", "Hernandez", "Jimenez", "Montes", "Arroyo", "Leal", "Ruiz", "Vasquez", 
+  "Solano", "Gonzalez", "Santos", "Lara", "Flores", "Martinez", "Ng", "Patel", "Desai", 
+  "Kaur", "Singh", "Sharma", "Choudhury", "Mishra", "Kumar", "Rai", "Bhatti", "Amin", 
+  "Ali", "Khan", "Hossain", "Reddy", "Yadav", "Singh", "Reddy", "Rai", "Bhatti", "Srinivasan", 
+  "Kaur", "Rani", "Bose", "Roy", "Chatterjee", "Bhattacharya", "Das", "Choudhury", "Basu", 
+  "Sarkar", "Bose", "Banerjee", "Mukherjee", "Chakraborty", "Mitra", "Dutta", "Ghosh", 
+  "Banerjee", "Nayak", "Haque", "Barua", "Chatterjee", "Basu", "Khan", "Mansoor", "Siddiqui", 
+  "Ranjan", "Srinivasan", "Iyer", "Dhanraj", "Dhar", "Gupta", "Chopra", "Mehta", "Puri", 
+  "Gupta", "Bhatia", "Luthra", "Arora", "Chand", "Kaur", "Bawa", "Rai", "Awasthi", "Chandra", 
+  "Dandekar", "Jadhav", "Rana", "Naik", "Pillai", "Raghavan", "Chakraborty", "Kumar", 
+  "Dutta", "Banerjee", "Chatterjee", "Mukherjee", "Roy", "Mishra", "Patel", "Nguyen", 
+  "Garcia", "Srinivasan", "Mishra", "Das", "Sharma", "Zhang", "Wang", "Li", "Gupta", 
+  "Cohen", "Benson", "Harrison", "Mason", "Chavez", "Liu", "Patel", "Reddy", "O'Reilly", 
+  "Singh", "Carson", "Chang", "Freeman", "Mathews", "McCarthy", "Baker", "Fletcher", 
+  "Holt", "Harris", "Palmer", "MacDonald", "Simpson", "Burns", "Morrison", "Dickson", 
+  "Watson", "Hall", "Baldwin", "Douglas", "Harrison", "Pearson", "Sampson", "Webb", 
+  "Gill", "Hunter", "Shaw", "Murray", "Powell", "Sutton", "Kennedy", "Stevens", "Stewart", 
+  "Wallace", "Hughes", "Lam", "Alvarez", "Maldonado", "Castillo", "Arroyo", "Mendez", 
+  "Ponce", "Lopez", "Salinas", "Rosario", "Salas", "Espinosa", "Marin", "Hernandez", 
+  "Cisneros", "Dominguez", "Rios", "Cantu", "Mendez", "Valencia", "Gonzalez", "Serrano", 
+  "Hernandez", "Vega", "Castaneda", "Benitez", "De La Cruz", "Calderon", "Gonzalez", 
+  "Espinoza", "Soto", "Mendez", "Miranda", "Garcia", "Marquez", "Gonzalez", "Zamora", 
+  "Alonso", "Barba", "Palacios", "Alvarado", "Santos", "Yamamoto", "Tanaka", "Nakamura", 
+  "Li", "Wang", "Zhang", "Chen", "Kawasaki", "Ishikawa", "Huang", "Saito", "Fujimoto", 
+  "Murakami", "Kobayashi", "Suzuki", "Sato", "Inoue", "Yamashita", "Aoki", "Takahashi", 
+  "Hayashi", "Taniguchi", "Shimizu", "Yamaguchi", "Nishimura", "Miyamoto", "Hirata", 
+  "Morita", "Ueno", "Kondo", "Nakano", "Hayashi", "Takahashi", "Sasaki", "Saito", 
+  "Park", "Lee", "Kim", "Choi", "Jung", "Kwon", "Lim", "Yoon", "Cho", "Hwang", 
+  "Yang", "Seo", "Bae", "Ahn", "Ryu", "Kim", "Noh", "Jeong", "Sung", "Hwang", 
+  "Na", "Gonzalez", "Bañuelos", "Cano", "Torres", "Nava", "Vázquez", "Yusupov", 
+  "Kovalenko", "Svetlova", "Petrov", "Popov", "Ivanov", "Smirnov", "Kovalev", "Orlov", 
+  "Romanov", "Sokolov", "Fedorov", "Mikhailov", "Kuznetsov", "Borisov", "Karpov", 
+  "Morozov", "Semyonov", "Novikov", "Vasiliev", "Filatov", "Semyonov", "Tikhomirov", 
+  "Dmitriev", "Grigory"
+];
+
+  console.log(docLastNames.length)
+
+var phoneCodes = [
+    "+1", "+7", "+20", "+27", "+30", "+31", "+32", "+33", "+34", "+36", "+39", "+40", "+41", "+44", "+45", "+46", "+47", "+48", "+49", 
+    "+51", "+52", "+53", "+54", "+55", "+56", "+57", "+58", "+60", "+61", "+62", "+63", "+64", "+65", "+66", "+81", "+82", "+84", "+86", 
+    "+90", "+91", "+92", "+93", "+94", "+95", "+98", "+212", "+213", "+216", "+218", "+220", "+221", "+233", "+234", "+255", "+256", 
+    "+260", "+263", "+264", "+290", "+297", "+298", "+299", "+350", "+351", "+352", "+353", "+354", "+355", "+356", "+357", "+358", 
+    "+359", "+370", "+371", "+372", "+373", "+374", "+375", "+376", "+377", "+380", "+381", "+382", "+383", "+385", "+386", "+387", 
+    "+389", "+420", "+421", "+423", "+500", "+501", "+502", "+503", "+504", "+505", "+506", "+507", "+509", "+590", "+591", "+592", 
+    "+593", "+594", "+595", "+596", "+597", "+598", "+599", "+675", "+676", "+677", "+678", "+679", "+680", "+681", "+682", "+685", 
+    "+686", "+687", "+688", "+689", "+850", "+852", "+853", "+855", "+856", "+880", "+886", "+960", "+961", "+962", "+963", "+964", 
+    "+965", "+966", "+967", "+968", "+970", "+971", "+972", "+973", "+974", "+975", "+976", "+977", "+992", "+993", "+994", "+995", 
+    "+996", "+998"
+  ];
+  
+
+
+  var finalPhone = [];
+  $("body").append(`
+    <div class="container">
+      <div class="name-col"></div>
+      <div class="number-col"></div>
+    </div>
+  `);
+
+function nameGerator (first, last, min, max, area) {
+  var a = 0;
+  while(a < 500) {
+  //generate name
+    
+    var randomFirstName = Math.floor(Math.random()*first);
+    var randomLastName = Math.floor(Math.random()*last);  
+    var finalName = `${docFirstNames[randomFirstName]} ${docLastNames[randomLastName]}`;  
+        i++
+        // $("body").append("<ul class='content names'><li>" + finalName + "</li></ul>")
+      
+
+
+        //number generator
+  var phoneNumber =[];
+  //phone length generator
+ const minCeiled = Math.ceil(min);
+ const maxFloored = Math.floor(max);
+ var phoneLength = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+ 
+   //picks a random area code
+ var areaCodeGenerator = Math.floor(Math.random()*area);
+ var finalPhoneCode = phoneCodes[areaCodeGenerator]
+
+ //generates a phone number based on the length
+ 
+   var i = 0;
+   while(i < phoneLength) {
+    
+       var numberGenerator = Math.floor(Math.random()*9); 
+      
+     
+     
+     phoneNumber.push(numberGenerator)
+     
+     i++;
+   }
+   
+ 
+ //addes the area code and to the numbers 
+ var phoneStr = phoneNumber.join("")
+ var finalN = (`${finalPhoneCode} ${phoneStr}`)
+ finalPhone.push(finalN)
+//  $("body").append("<ul class='content numbers'><li>" + finalN + "</li></ul>")
+
+$(".name-col").append(`<div class='name'>${finalName}</div>`);
+    $(".number-col").append(`<div class='number'>${finalN}</div>`);
+
+a++;
+}
+}
+
+ 
+  
+nameGerator(docFirstNames.length, docLastNames.length, 7, 15, phoneCodes.length);
+
+
+
+
+
+
+
+// function phoneNumberGenerator(min, max, area) {
+ 
+//   var phoneNumber =[];
+//    //phone length generator
+//   const minCeiled = Math.ceil(min);
+//   const maxFloored = Math.floor(max);
+//   var phoneLength = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  
+//     //picks a random area code
+//   var areaCodeGenerator = Math.floor(Math.random()*area);
+//   var finalPhoneCode = phoneCodes[areaCodeGenerator]
+ 
+//   //generates a phone number based on the length
+  
+//     var i = 0;
+//     while(i < phoneLength) {
+     
+//         var numberGenerator = Math.floor(Math.random()*9); 
+       
+      
+      
+//       phoneNumber.push(numberGenerator)
+      
+//       i++;
+//     }
+    
+  
+//   //addes the area code and to the numbers 
+//   var phoneStr = phoneNumber.join("")
+//   var finalN = (`${finalPhoneCode} ${phoneStr}`)
+//   finalPhone.push(finalN)
+//   $("body").append("<ul class='content numbers'><li>" + finalN + "</li></ul>")
+// console.log(finalN)
+// }
+// var a = 0;
+//   while(a < 100) {
+// phoneNumberGenerator(7, 15, phoneCodes.length)
+
+//     a++;
+//     }
+
+
+
+
+  //   function nameGerator (first, last) {
+  //     var i = 0;
+  //     while(i < 100) {
+  //     var randomFirstName = Math.floor(Math.random()*first);
+  //     var randomLastName = Math.floor(Math.random()*last);  
+  //     var finalName = `${docFirstNames[randomFirstName]} ${docLastNames[randomLastName]}`;  
+      
+  //     // console.log(finalName)
+          
+  //         i++
+  //         // $(".docName").after("<ol><li>" + finalName + "</li></ol>")
+  //         $("body").append("<ul class='content names'><li>" + finalName + "</li></ul>")
+  //         // $("body").after("<ol>" + finalName + "</ol>")
+  //     }
+  //     // alert(`the doctor of the day is.....${finalName}` )
+  // }
+  // nameGerator(docFirstNames.length, docLastNames.length);
+  // var finalPhone = [];
+  
+  // function phoneNumberGenerator(min, max, area) {
+   
+  //   var phoneNumber =[];
+  //    //phone length generator
+  //   const minCeiled = Math.ceil(min);
+  //   const maxFloored = Math.floor(max);
+  //   var phoneLength = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    
+  //     //picks a random area code
+  //   var areaCodeGenerator = Math.floor(Math.random()*area);
+  //   var finalPhoneCode = phoneCodes[areaCodeGenerator]
+   
+  //   //generates a phone number based on the length
+    
+  //     var i = 0;
+  //     while(i < phoneLength) {
+       
+  //         var numberGenerator = Math.floor(Math.random()*9); 
+         
+        
+        
+  //       phoneNumber.push(numberGenerator)
+        
+  //       i++;
+  //     }
+      
+    
+  //   //addes the area code and to the numbers 
+  //   var phoneStr = phoneNumber.join("")
+  //   var finalN = (`${finalPhoneCode} ${phoneStr}`)
+  //   finalPhone.push(finalN)
+  //   $("body").append("<ul class='content numbers'><li>" + finalN + "</li></ul>")
+  // console.log(finalN)
+  // }
+  // var a = 0;
+  //   while(a < 100) {
+  // phoneNumberGenerator(7, 15, phoneCodes.length)
+  
+  //     a++;
+  //     }
